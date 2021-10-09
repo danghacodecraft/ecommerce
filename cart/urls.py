125f7b33d0f10a9e5992aa257c1c2d4a52b124a6
hadang <hadh99sg@gmail.com>
2021-10-10 00:57:14 +0700

    commit all

--- cart/urls.py
+++ cart/urls.py
@@ -4,7 +4,7 @@ from cart.views import *
 
 app_name = 'cart'
 urlpatterns = [
-    path('cart/', cart_detail, name='cart_detail'),
+    path('gio-hang/', cart_detail, name='cart_detail'),
     path('buy_now/<int:product_id>/', buy_now, name='buy_now'),
     path('remove_cart/<int:product_id>/', remove_cart, name='remove_cart'),
 ]
