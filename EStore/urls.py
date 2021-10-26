24b914dbea21552fa15328ef70f526a608b501d6
hadang <hadh99sg@gmail.com>
2021-10-26 22:49:51 +0700

    commit all

--- EStore/urls.py
+++ EStore/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 # Static
 from django.conf.urls.static import static
 from django.conf import settings
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', include('customers.urls')),
     path('', include('cart.urls')),
     path('', include('checkout.urls')),
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
 # Static
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
